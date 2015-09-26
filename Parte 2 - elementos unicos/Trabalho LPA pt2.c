247c93eaa2d501b5b5fd23eb7d51424844b3fdc1
luisusp2015 <lipemoreira10@hotmail.com>
2015-09-26 19:08:50 -0300

    Trabalho LPA - 2015 v1.6

--- Parte 2 - elementos unicos/Trabalho LPA pt2.c
+++ Parte 2 - elementos unicos/Trabalho LPA pt2.c
@@ -48,7 +48,7 @@ void identificador(int vetor[],int tamvet){
 	int i=0;
 	int q=0;
 	int w=0;
-	int k,j ;
+	int k,j , r;
 	int *vetu;
 	vetu = (int*)calloc(tamvet,sizeof (int));//usando o calloc para preencher os espaços vazios com o valor 0;
 	/* iremos separar a situação em 2 casos
@@ -63,20 +63,25 @@ void identificador(int vetor[],int tamvet){
 			j = i+1 ; 
 			if(vetor[i]==vetor[j]){//cria um vetor formado pelos valores repetidos em suas respectivas posições de origem
 				vetu[i] = vetor[i];
-				vetu[j] = vetor[j];
+				vetu[j] = vetor[i];
 			}	
 		}
 		for(q;q<tamvet;q++){
+		    r = q+1;
+		    if(vetor[q]==0){
+			}
 			k = vetor[q] - vetu[q];//realiza a subtração vetorial para eliminar os repetidos
 			if(vetor[q]!=0 && k!=0){//para restringir  o caso especial em que  o elemento unico é o valor zero;
-			if(k!= 0){ // imprime os valores que são != 0  , ou seja, não repetidos , unicos
-				printf("%d ",vetor[q]);
-				w++; // cria contador para verificar os casos em q nao existem elementos unicos;
-			}
-		} if(vetor[q]!= vetor[q+1]&&vetor[q]==0){ // caso especial em que o elemento unico é o zero;
-			printf("%d ",vetor[q]);
+				printf("%d ",vetor[q]);// imprime os valores que são != 0  , ou seja, não repetidos , unicos
+				w++; // cria contador para verificar os casos em que nao existem elementos unicos;
+			}		
+				if(vetor[q] != vetor[r] && vetor[q]!=vetor[r+1]){
+		             if( vetor[q]==0){ // caso especial em que o elemento unico é o zero;
+			             printf("%d ",vetor[q]);
+			             }
+	
 		}
-		}
+	}
 			if(w==0){  
 				printf("\aNao existem elementos unicos no conjunto!");				
 			}
